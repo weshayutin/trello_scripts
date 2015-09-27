4c013b3a9389ac2686bf018d226b5059ebeda9a7
Wesley Hayutin <weshayutin@gmail.com>
2015-09-26 20:53:58 -0400

    add time since creation test

--- test_trolly.py
+++ test_trolly.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
-
+import dateutil.parser
 from datetime import datetime
+from dateutil.relativedelta import *
 import os
 import trolly
+
+import pdb
 
 #Trello API_KEY, trello TOKEN
 client = trolly.client.Client(os.environ['API_KEY'], os.environ['TOKEN'])
 openstack_ci = [board for board in client.get_boards() if board.name == "openstack-ci"][0]
+
+#get the current time
+now = datetime.utcnow()
 
 cards = openstack_ci.get_cards()
 
@@ -16,9 +22,21 @@ cards_in_progress = []
 print 'There are %s cards in progress' % len(cards_in_progress)
 for card in cards_in_progress:
     id = card.id
-    created_on = datetime.fromtimestamp(int(id[0:8],16))
-    print '%s was created on %s' %(card.name, created_on)
-#print(cards)
+    hex_date = (int(id[0:8],16))
+    created_on_human = unicode(datetime.fromtimestamp(hex_date))
+    #created_on = datetime.strptime(created_on_human, '%Y-%m-%d %H:%M:%S')
+    created_on = dateutil.parser.parse(created_on_human)
+    delta = relativedelta(now, created_on)
+    #pdb.set_trace()
+    members = card.get_members()
+    if len(members) > 1:
+       member_list = []
+       for member in members:
+           #pdb.set_trace()
+           member_list.append(member.name)
+    else:
+        member_list = members[0].name
+    if delta.months > 0:
+        print '%s is marked in progress but is %s months old. Owner(s) are %s ' % (card.name, delta.months, str(member_list))
 
 
-#Update rdopkg workflow [src_gate]
