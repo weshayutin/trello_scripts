0db7b2a7187f11e6ec2cfac6a39c5c08b87ef5e9
Wesley Hayutin <weshayutin@gmail.com>
2015-09-28 11:01:47 -0400

    create functions and cleanup

--- test_trolly.py
+++ test_trolly.py
@@ -20,65 +20,70 @@ email_server = smtplib.SMTP('smtp.corp.redhat.com', 25)
 client = trolly.client.Client(os.environ['API_KEY'], os.environ['TOKEN'])
 openstack_ci = [board for board in client.get_boards() if board.name == "openstack-ci"][0]
 
+def email_send(email_from, email_to, subject, body):
+    email = MIMEMultipart()
+    email['From'] = email_from
+    email['To'] = email_to
+    email['Subject'] = subject
+    body = body
+    email.attach(MIMEText(body,'plain'))
+    text = email.as_string()
+    email_server.sendmail(email_from, email_to, text)
+
+def generate_report():
+    for card in cards_in_progress:
+        id = card.id
+        #Determine the creation date of the card
+        hex_date = (int(id[0:8],16))
+        created_on_human = unicode(datetime.fromtimestamp(hex_date))
+        created_on = dateutil.parser.parse(created_on_human)
+        delta = relativedelta(now, created_on)
+        members = card.get_members()
+        if len(members) > 1:
+           member_list = []
+           for member in members:
+               if team.has_key(member.name):
+                   member_list.append(member.name)
+           member_list_str = (", ".join(member_list))
+        else:
+            member_list_str = members[0].name
+        #convert member_list_str back into a list
+        member_list = member_list_str.split(",")
+
+        if delta.months > 0:
+            msg = 'CARD URL: %s\nNAME: %s\n Card is marked in progress but is %s months old.\n Owner(s) are %s\n ' % (card.get_card_information()['url'], card.name, delta.months, member_list_str)
+            for member in member_list:
+                msg_dict[str(member).strip()].append(msg)
+        if delta.months == 0 and delta.days > 7:
+            msg = 'CARD URL: %s\nNAME: %s\n Card is marked in progress but is %s days old.\n Owner(s) are %s\n ' % (card.get_card_information()['url'], card.name, delta.days, member_list_str)
+            for member in member_list:
+                msg_dict[str(member).strip()].append(msg)
+
+
 #get the current time
 now = datetime.utcnow()
 
+# get card list
 cards = openstack_ci.get_cards()
-
 cards_in_progress = []
 [cards_in_progress.append(card) for card in cards if card.get_list().name == "In Progress"]
-
-print 'There are %s cards in progress' % len(cards_in_progress)
 
 #create a dict, key = member name and list for values
 msg_dict = {}
 for key in team.iterkeys():
     msg_dict[key] = []
 
-for card in cards_in_progress:
-    id = card.id
-    #Determine the creation date of the card
-    hex_date = (int(id[0:8],16))
-    created_on_human = unicode(datetime.fromtimestamp(hex_date))
-    created_on = dateutil.parser.parse(created_on_human)
-    delta = relativedelta(now, created_on)
-    members = card.get_members()
-    if len(members) > 1:
-       member_list = []
-       for member in members:
-           if team.has_key(member.name):
-               member_list.append(member.name)
-       member_list_str = (", ".join(member_list))
-    else:
-        member_list_str = members[0].name
-    #convert member_list_str back into a list
-    member_list = member_list_str.split(",")
+#Generate Report
+generate_report()
 
-    if delta.months > 0:
-        msg = 'CARD URL: %s\nNAME: %s\n Card is marked in progress but is %s months old.\n Owner(s) are %s\n ' % (card.get_card_information()['url'], card.name, delta.months, member_list_str)
-        for member in member_list:
-            msg_dict[str(member).strip()].append(msg)
-    if delta.months == 0 and delta.days > 7:
-        msg = 'CARD URL: %s\nNAME: %s\n Card is marked in progress but is %s days old.\n Owner(s) are %s\n ' % (card.get_card_information()['url'], card.name, delta.days, member_list_str)
-        for member in member_list:
-            msg_dict[str(member).strip()].append(msg)
-
+#EMAIL SECTION
 email_list = ast.literal_eval(os.environ['TEAM_TO_EMAIL'])
 email_server.starttls()
 
 #email each owner w/ a list of cards that require attention
 for name, msg in msg_dict.iteritems():
     if name in email_list:
-      email_from = "whayutin@redhat.com"
-      email_to = email_list[name]
-      email = MIMEMultipart()
-      email['From'] = email_from
-      email['To'] = email_to
-      email['Subject'] = "[trello report] Trello cards need attention"
-      body = '\n\n'.join(msg)
-      email.attach(MIMEText(body,'plain'))
-      text = email.as_string()
-      email_server.sendmail(email_from, email_to, text)
+      email_send("whayutin@redhat.com", email_list[name], "[trello report] Trello cards that need attention", '\n\n'.join(msg))
 
 #email report
 all_msg = ""
@@ -88,17 +93,9 @@ for name, msg in msg_dict.iteritems():
       all_msg += '\n\n'
       all_msg += '\n'.join(msg)
 
-email_from = "whayutin@redhat.com"
-email_to = "whayutin@redhat.com"
-email = MIMEMultipart()
-email['From'] = email_from
-email['To'] = email_to
-email['Subject'] = "[trello rollup report] Trello cards need attention"
-#body = '\n\n'.join(all_msg)
-body = all_msg
-email.attach(MIMEText(body,'plain'))
-text = email.as_string()
-email_server.sendmail(email_from, email_to, text)
+email_send("whayutin@redhat.com", "whayutin@redhat.com", "[trello rollup report] Trello cards that need attention", all_msg)
 
+
+#shutdown the connection to smtp/email
 email_server.quit()
 
